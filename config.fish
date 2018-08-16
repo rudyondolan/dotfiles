8a9ea737b2b134aa803e2a9044b3dd7c6ad6bef7
Rudy Ondolan <rudy.ondolan@gmail.com>
2018-08-16 15:40:07 +0700

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,27 +1,38 @@
-# Path to Oh My Fish install.
-set -q XDG_DATA_HOME
-  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
+# put this files on $HOME/.config/fish/config.fish
+# Theme Bobthefish
+# set -g theme_color_scheme base16-dark
+# set -g theme_color_scheme solarized-dark
+set -g theme_color_scheme dark
+# set -g theme_powerline_fonts no
+set -g theme_nerd_fonts yes
+set -g theme_display_date no
+# Theme Default
+set -g theme_short_path yes
 
-# Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
+# PATH
+set -g -x PATH /usr/local/bin $PATH
+set -g -x PATH /usr/local/sbin $PATH
+set -g -x PATH $HOME/Library/Android/sdk $PATH
+set -g -x PATH $HOME/Library/Android/sdk/tools $PATH
+set -g -x PATH $HOME/Library/Android/sdk/tools/bin $PATH
+set -g -x PATH $HOME/Library/Android/sdk/platform-tools $PATH
 
 # Remove greeting message
 set fish_greeting ''
 
-# Theme
-set theme_color_scheme solarized-light
-set -g theme_color_scheme light
-set -g theme_nerd_fonts yes
-set -g theme_powerline_fonts yes
+# Alias
+alias a=atom
+alias m=mvim
+alias t=trash
+alias v=nvim
+alias vs=code-insiders
+alias de="cd $HOME/Desktop"
+alias wo="cd $HOME/Works"
+alias so="source $HOME/.config/fish/config.fish"
+alias myfish="v $HOME/.config/fish/config.fish"
+alias aether="cd $HOME/Works/tessereact-ui-apollo-frontend & v"
 
-# Powerline
-# set fish_function_path $fish_function_path "/home/rudy/.local/lib/python3.5/site-packages/powerline/bindings/fish"
-# powerline-setup
-
-# Laravel
-set -gx PATH $PATH "/home/rudy/.config/composer/vendor/bin"
-
-# PHPbrew
-set -gx PHPBREW /home/rudy/.phpbrew/phpbrew.fish
-
+# Base16 Shell
+# if status --is-interactive
+#     eval sh $HOME/.config/base16-shell/scripts/base16-tomorrow-night.sh
+# end
